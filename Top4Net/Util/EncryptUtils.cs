85fde9306891fa09f58a674153ac1b30bfd3e561
carver.gu <carver.gu@903f60ec-79e3-11de-98bd-fdc22f85cdda>
2009-07-27 13:23:22 +0000

    provide request signature method

---
+++ Top4Net/Util/EncryptUtils.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace Taobao.Top.Api.Util
+{
+    public abstract class EncryptUtils
+    {
+        /// <summary>
+        /// 给TOP请求签名。
+        /// </summary>
+        /// <param name="parameters">所有字符型的TOP请求参数</param>
+        /// <param name="secret">签名密钥</param>
+        /// <returns>签名</returns>
+        public static string SignTopRequest(IDictionary<string, string> parameters, string secret)
+        {
+            // 第一步：把字典按Key的字母顺序排序
+            IDictionary<string, string> sortedParams = new SortedDictionary<string, string>(parameters);
+            IEnumerator<KeyValuePair<string, string>> dem = sortedParams.GetEnumerator();
+
+            // 第二步：把所有参数名和参数值串在一起
+            StringBuilder query = new StringBuilder(secret);
+            while (dem.MoveNext())
+            {
+                query.Append(dem.Current.Key).Append(dem.Current.Value);
+            }
+
+            Console.WriteLine(query.ToString());
+
+            // 第三步：使用MD5加密
+            MD5 md5 = MD5.Create();
+            byte[] bytes = md5.ComputeHash(UTF8Encoding.Default.GetBytes(query.ToString()));
+
+            // 第四步：把二进制转化为大写的十六进制
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                result.Append(bytes[i].ToString("X"));
+            }
+
+            Console.WriteLine(result.ToString());
+            return result.ToString();
+        }
+    }
+}
