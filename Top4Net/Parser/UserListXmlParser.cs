e91da6b5b7f60d9d5ede59671238a7a5f1ecbc8d
carver.gu <carver.gu@903f60ec-79e3-11de-98bd-fdc22f85cdda>
2009-07-27 15:34:51 +0000

    办公室里面已经没人，我一个人奋战到23:30终于把这个变态的XML解释搞好了。

---
+++ Top4Net/Parser/UserListXmlParser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+using Taobao.Top.Api.Domain;
+using System.Text;
+
+namespace Taobao.Top.Api.Parser
+{
+    /// <summary>
+    /// TOP API: taobao.users.get的XML响应解释器。
+    /// </summary>
+    public class UserListXmlParser : ITopParser<List<User>>
+    {
+        #region ITopParser<List<User>> Members
+
+        public List<User> Parse(string body)
+        {
+            XmlAttributes attrs = new XmlAttributes();
+            attrs.XmlElements.Add(new XmlElementAttribute("user", typeof(User)));
+
+            XmlAttributeOverrides attrOvrs = new XmlAttributeOverrides();
+            attrOvrs.Add(typeof(UserListResponse), "Users", attrs);
+
+            XmlSerializer serializer = new XmlSerializer(typeof(UserListResponse), attrOvrs);
+
+            object obj = serializer.Deserialize(new StringReader(body));
+            UserListResponse ulr = obj as UserListResponse;
+            return ulr == null ? null : ulr.Users;
+        }
+
+        #endregion
+
+        [Serializable]
+        [XmlRoot("rsp")]
+        public class UserListResponse
+        {
+            public List<User> Users { get; set; }
+        }
+
+    }
+}
