9e607dc851ec9e948339f49fac511d2060aec635
carver.gu <carver.gu@903f60ec-79e3-11de-98bd-fdc22f85cdda>
2009-07-27 13:24:12 +0000

    Add new unit test class for Entry Utilities class

---
+++ Top4NetTest/EncryptUtilsTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Taobao.Top.Api.Util;
+
+namespace Taobao.Top.Api.Test
+{
+    [TestClass]
+    public class EncryptUtilsTest
+    {
+        [TestMethod]
+        public void SignTopRequest()
+        {
+            IDictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("fields", "nick,sex");
+            parameters.Add("nick", "winwindg");
+            string signature = EncryptUtils.SignTopRequest(parameters, "carver");
+            Assert.AreEqual("E780A69E4E438846FAF25AD2847415", signature);
+        }
+    }
+}
