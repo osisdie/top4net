a67d604dc7c74d461f808dfe97dc97d2fce90dc1
carver.gu <carver.gu@903f60ec-79e3-11de-98bd-fdc22f85cdda>
2009-07-28 16:32:37 +0000

    update assembly information

--- Top4Net/Properties/AssemblyInfo.cs
+++ Top4Net/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TopNetSdk")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Top4Net")]
+[assembly: AssemblyDescription(".NET SDK for TOP")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Taobao")]
-[assembly: AssemblyProduct("TopNetSdk")]
-[assembly: AssemblyCopyright("Copyright © Taobao 2009")]
+[assembly: AssemblyCompany("Carver Gu")]
+[assembly: AssemblyProduct("Top4Net")]
+[assembly: AssemblyCopyright("Copyright © Carver Gu 2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
