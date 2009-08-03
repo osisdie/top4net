d94061c6f686190ab94c266bfa462ebd7a918821
carver.gu <carver.gu@903f60ec-79e3-11de-98bd-fdc22f85cdda>
2009-08-03 14:55:20 +0000

    1. add two proxy classes for sandbox testing

---
+++ Top4NetTest/Request/TopPrivateRequestProxy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Taobao.Top.Api.Request;
+
+namespace Taobao.Top.Api.Test
+{
+    /// <summary>
+    /// TOP私有API请求代理。
+    /// </summary>
+    public class TopPrivateRequestProxy : ITopPrivateRequest
+    {
+        private ITopPrivateRequest request;
+        private string nick;
+
+        public TopPrivateRequestProxy(ITopPrivateRequest request, string nick)
+        {
+            this.request = request;
+            this.nick = nick;
+        }
+
+        #region ITopRequest Members
+
+        public string GetApiName()
+        {
+            return request.GetApiName();
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            IDictionary<string, string> parameters = request.GetParameters();
+            parameters.Add("session_nick", this.nick);
+            parameters.Add("session_id", Guid.NewGuid().ToString());
+            return parameters;
+        }
+
+        #endregion
+
+        #region ITopPrivateRequest Members
+
+        public string GetSessionKey()
+        {
+            return request.GetSessionKey();
+        }
+
+        #endregion
+    }
+}
