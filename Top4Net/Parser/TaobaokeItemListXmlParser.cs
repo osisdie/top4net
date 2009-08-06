e35377ca979d524786252cdc9c2bd8f77572be39
carver.gu <carver.gu@903f60ec-79e3-11de-98bd-fdc22f85cdda>
2009-08-06 14:56:30 +0000

    refine new parser classes

--- Top4Net/Parser/TaobaokeItemListXmlParser.cs
+++ Top4Net/Parser/TaobaokeItemListXmlParser.cs
@@ -1,31 +1,34 @@
 ﻿using System;
 using System.IO;
-using System.Collections.Generic;
 using System.Xml.Serialization;
+using System.Collections.Generic;
 
 using Taobao.Top.Api.Domain;
 
 namespace Taobao.Top.Api.Parser
 {
+    /// <summary>
+    /// 淘宝客商品列表的XML响应解释器。
+    /// </summary>
     public class TaobaokeItemListXmlParser : ITopParser<List<TaobaokeItem>>
     {
         public List<TaobaokeItem> Parse(string body)
         {
-            XmlSerializer serializer = new XmlSerializer( typeof( TaobaokeItemListResponse ) );
+            XmlSerializer serializer = new XmlSerializer(typeof(TaobaokeItemListResponse));
 
-            object obj = serializer.Deserialize( new StringReader( body ) );
+            object obj = serializer.Deserialize(new StringReader(body));
             TaobaokeItemListResponse tir = obj as TaobaokeItemListResponse;
             return tir == null ? null : tir.TaobaokeItems;
         }
 
         /// <summary>
-        /// 产品列表响应，用于XML反序列化。
+        /// 淘宝客商品列表响应，用于XML反序列化。
         /// </summary>
         [Serializable]
-        [XmlRoot( "rsp" )]
+        [XmlRoot("rsp")]
         public class TaobaokeItemListResponse
         {
-            [XmlElement( "taobaokeItem" )]
+            [XmlElement("taobaokeItem")]
             public List<TaobaokeItem> TaobaokeItems { get; set; }
         }
     }
--- Top4Net/Parser/TaobaokeItemXmlParser.cs
+++ Top4Net/Parser/TaobaokeItemXmlParser.cs
@@ -7,7 +7,7 @@ using Taobao.Top.Api.Domain;
 namespace Taobao.Top.Api.Parser
 {
     /// <summary>
-    /// 
+    /// 淘宝客商品的XML响应解释器。
     /// </summary>
     public class TaobaokeItemXmlParser : ITopParser<TaobaokeItem>
     {
@@ -15,21 +15,21 @@ namespace Taobao.Top.Api.Parser
 
         public TaobaokeItem Parse(string body)
         {
-            XmlSerializer serializer = new XmlSerializer( typeof( TaobaokeItemResponse ) );
-            object obj = serializer.Deserialize( new StringReader( body ) );
-            return ( obj as TaobaokeItemResponse ).TaobaokeItem;
+            XmlSerializer serializer = new XmlSerializer(typeof(TaobaokeItemResponse));
+            object obj = serializer.Deserialize(new StringReader(body));
+            return (obj as TaobaokeItemResponse).TaobaokeItem;
         }
 
         #endregion
 
         /// <summary>
-        /// 
+        /// 淘宝客商品响应，用于XML反序列化。
         /// </summary>
         [Serializable]
-        [XmlRoot( "rsp" )]
+        [XmlRoot("rsp")]
         public class TaobaokeItemResponse
         {
-            [XmlElement( "taobaokeitem" )]
+            [XmlElement("taobaokeitem")]
             public TaobaokeItem TaobaokeItem { get; set; }
         }
     }
