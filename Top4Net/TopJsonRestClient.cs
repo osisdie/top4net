7df99b64dc87cd233749dd21414097ade82387bc
carver.gu <carver.gu@903f60ec-79e3-11de-98bd-fdc22f85cdda>
2009-08-24 15:54:00 +0000

    top json rest client helper class

---
+++ Top4Net/TopJsonRestClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Taobao.Top.Api.Request;
+using Taobao.Top.Api.Parser;
+using Taobao.Top.Api.Domain;
+
+namespace Taobao.Top.Api
+{
+    /// <summary>
+    /// TOP基于REST的JSON客户端帮助类。
+    /// </summary>
+    public class TopJsonRestClient
+    {
+        private ITopClient client;
+
+        public TopJsonRestClient(string topUrl, string appKey, string appSecret)
+        {
+            client = new TopRestClient(topUrl, appKey, appSecret, TopRestClient.FORMAT_JSON);
+        }
+
+        public User getUser(ITopRequest request, string session)
+        {
+            return client.Execute(request, new UserJsonParser(), session);
+        }
+
+        public ResponseList<User> getUsers(ITopRequest request)
+        {
+            return client.Execute(request, new UserListJsonParser());
+        }
+    }
+}
