f749c9461bdce23e9fad5b6b97036e19e5caf27b
icyker@gmail.com <icyker@gmail.com@903f60ec-79e3-11de-98bd-fdc22f85cdda>
2009-08-25 04:13:39 +0000

    add shopParser

---
+++ Top4Net/Parser/ShopXmlParser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Taobao.Top.Api.Domain;
+
+namespace Taobao.Top.Api.Parser
+{
+    /// <summary>
+    /// 店铺的XML响应解释器
+    /// </summary>
+    public class ShopXmlParser : ITopParser<Shop>
+    {
+        #region ITopParser<Shop> Members
+
+        public Shop Parse(string body)
+        {
+            ShopListXmlParser parser = new ShopListXmlParser();
+            return parser.Parse( body ).GetFirst();
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// 店铺列表的XML响应解释器。
+    /// </summary>
+    public class ShopListXmlParser : ITopParser<ResponseList<Shop>>
+    {
+        #region ITopParser<ResponseList<Product>> Members
+
+        public ResponseList<Shop> Parse(string body)
+        {
+            return ResponseList<Shop>.ParseXmlResponse( "shop", body );
+        }
+
+        #endregion
+    }
+}
