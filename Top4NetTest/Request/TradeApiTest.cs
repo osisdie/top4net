5f87f540677715325eca68cb68b52a340667ca9d
carver.gu <carver.gu@903f60ec-79e3-11de-98bd-fdc22f85cdda>
2009-08-27 10:30:48 +0000

    new test case

--- Top4NetTest/Request/TradeApiTest.cs
+++ Top4NetTest/Request/TradeApiTest.cs
@@ -27,5 +27,17 @@ namespace Taobao.Top.Api.Test.Request
             ITopClient client = TestUtils.GetDevelopTopClient("json");
             TradeDetailGetRequest req = new TradeDetailGetRequest();
         }
+
+        [TestMethod]
+        public void GetRefundByJson()
+        {
+            ITopClient client = TestUtils.GetDevelopTopClient("xml");
+            RefundGetRequest req = new RefundGetRequest();
+            req.Fields = "refund_id,address";
+            req.Rid = "126628";
+            ITopRequest proxy = new TopRequestProxy(req, "tbtest1202");
+            string rsp = client.Execute(proxy, new StringParser());
+            Console.WriteLine(rsp);
+        }
     }
 }
