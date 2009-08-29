456813a4f77e61b9a34153a12fb8aba64042d6f6
carver.gu <carver.gu@903f60ec-79e3-11de-98bd-fdc22f85cdda>
2009-08-29 02:34:22 +0000

    add shop related json/xml parsers and tests

---
+++ Top4Net/Parser/ShopJsonParser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Taobao.Top.Api.Domain;
+
+namespace Taobao.Top.Api.Parser
+{
+    /// <summary>
+    /// 店铺的JSON响应解释器。
+    /// </summary>
+    public class ShopJsonParser : ITopParser<Shop>
+    {
+        #region ITopParser<Shop> Members
+
+        public Shop Parse(string body)
+        {
+            ShopListJsonParser parser = new ShopListJsonParser();
+            return parser.Parse(body).GetFirst();
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// 店铺列表的JSON响应解释器。
+    /// </summary>
+    public class ShopListJsonParser : ITopParser<ResponseList<Shop>>
+    {
+        #region ITopParser<ResponseList<Shop>> Members
+
+        public ResponseList<Shop> Parse(string body)
+        {
+            return ResponseList<Shop>.ParseJsonResponse("shops", body);
+        }
+
+        #endregion
+    }
+}
---
+++ Top4NetTest/Parser/ShopParserTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Taobao.Top.Api.Parser;
+using Taobao.Top.Api.Domain;
+
+namespace Taobao.Top.Api.Test.Parser
+{
+    [TestClass]
+    public class ShopParserTest
+    {
+        [TestMethod]
+        public void ParseXmlAsShops()
+        {
+            ShopListXmlParser parser = new ShopListXmlParser();
+            string body = TestUtils.GetResourceAsText("shops.xml");
+            ResponseList<Shop> rsp = parser.Parse(body);
+            Assert.IsNotNull(rsp.Content);
+            Assert.AreEqual(1, rsp.Content.Count);
+            Assert.AreEqual("44696469", rsp.Content[0].Sid);
+            Assert.AreEqual("tbtest561", rsp.Content[0].SellerNick);
+        }
+
+        [TestMethod]
+        public void ParseJsonAsShop()
+        {
+            ShopListJsonParser parser = new ShopListJsonParser();
+            string body = TestUtils.GetResourceAsText("shops.json");
+            ResponseList<Shop> rsp = parser.Parse(body);
+            Assert.IsNotNull(rsp.Content);
+            Assert.AreEqual(1, rsp.Content.Count);
+            Assert.AreEqual("店铺介绍", rsp.Content[0].Description);
+            Assert.AreEqual("店铺名称", rsp.Content[0].Title);
+            Assert.AreEqual("How are you doing?", rsp.Content[0].Bulletin);
+        }
+
+    }
+}
