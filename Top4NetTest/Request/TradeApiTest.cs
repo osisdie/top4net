1125bad93c546d43c58e3c8738889dede350122c
carver.gu <carver.gu@903f60ec-79e3-11de-98bd-fdc22f85cdda>
2010-04-14 06:32:34 +0000

    develop top4net 2.0

---
+++ Top4NetTest/Request/TradeApiTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Taobao.Top.Api.Domain;
+using Taobao.Top.Api.Request;
+
+namespace Taobao.Top.Api.Test.Request
+{
+    [TestClass]
+    public class TradeApiTest
+    {
+        [TestMethod]
+        public void GetSoldTrades()
+        {
+            TopXmlRestClient client = TestUtils.GetProductTopClient();
+            TradesSoldGetRequest req = new TradesSoldGetRequest();
+            req.Fields = "tid,buyer_nick,seller_nick,modified,orders.iid,orders.title,orders.price";
+            PageList<Trade> rsp = client.TradesSoldGet(req);
+            Console.WriteLine(rsp.TotalResults);
+        }
+    }
+}
---
+++ Top4NetTest/Request/UserApiTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Taobao.Top.Api.Domain;
+using Taobao.Top.Api.Request;
+
+namespace Taobao.Top.Api.Test.Request
+{
+    [TestClass]
+    public class UserApiTest
+    {
+        [TestMethod]
+        public void GetUser()
+        {
+            TopXmlRestClient client = TestUtils.GetTestingTopClient();
+            UserGetRequest req = new UserGetRequest();
+            req.Fields = "nick,sex,location";
+            req.Nick = "tbtest1063";
+            User user = client.UserGet(req);
+            Console.WriteLine(user.Nick);
+        }
+    }
+}
