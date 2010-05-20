3d01293f9de8455415abd4b1e40946f22ea6e054
carver.gu <carver.gu@903f60ec-79e3-11de-98bd-fdc22f85cdda>
2010-05-20 12:20:59 +0000

    add new test cases

---
+++ Top4NetTest/Request/ShopApiTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Taobao.Top.Api.Domain;
+using Taobao.Top.Api.Request;
+
+namespace Taobao.Top.Api.Test.Request
+{
+    [TestClass]
+    public class ShopApiTest
+    {
+        private TopXmlRestClient client = TestUtils.GetProductTopClient();
+
+        [TestMethod]
+        public void GetShopRemainShowCase()
+        {
+            ShopRemainshowcaseGetRequest req = new ShopRemainshowcaseGetRequest();
+            Shop shop = client.ShopRemainshowcaseGet(req, "xxx");
+            Console.WriteLine(shop.Nick);
+        }
+    }
+}
