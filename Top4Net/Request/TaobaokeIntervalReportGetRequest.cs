8994766d1352b0ba74f17977479a91a9d223957b
icyker@gmail.com <icyker@gmail.com@903f60ec-79e3-11de-98bd-fdc22f85cdda>
2010-07-25 04:18:00 +0000

    add TaobaokeIntervalReportGetRequest.cs

---
+++ Top4Net/Request/TaobaokeIntervalReportGetRequest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Taobao.Top.Api.Request
+{
+    /// <summary>
+    /// TOP API: taobao.taobaoke.interval.report.get
+    /// add by icyker@gmail.com at 2010年6月19日18:24:22
+    /// </summary>
+    public class TaobaokeIntervalReportGetRequest : ITopRequest
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        #region ITopRequest Members
+
+        public string GetApiName()
+        {
+            return "taobao.taobaoke.interval.report.get";
+        }
+
+        public IDictionary<string, string> GetParameters()
+        {
+            TopDictionary parameters = new TopDictionary();
+            parameters.Add( "start_date", this.StartDate.ToString( "yyyy-MM-dd HH:mm:ss" ) );
+            parameters.Add( "end_date", this.EndDate.ToString( "yyyy-MM-dd HH:mm:ss" ) );
+            return parameters;
+        }
+
+        #endregion
+    }
+}
